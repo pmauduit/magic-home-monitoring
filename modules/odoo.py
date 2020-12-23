21a74748139a44625fd1a8b3e7b9cfaa3f650255
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2020-12-23 18:20:45 +0100

    catching errors when communicating with odoo

--- modules/odoo.py
+++ modules/odoo.py
@@ -1,4 +1,5 @@
-import datetime
+import logging
+import urllib
 from datetime import date, timedelta, datetime
 
 import odoorpc
@@ -21,29 +22,33 @@ class odoo:
                                                self.user_id, ['employee_ids'])[0]['employee_ids'][0]
 
     def get_total_attendance(self, period="daily"):
-        if period == "daily":
-            begin_midnight = "{}T00:00:00.0Z".format(date.today())
-            end_midnight = "{}T00:00:00.0Z".format(date.today() + timedelta(days=1))
-        else:
-            today = date.today()
-            last_monday = today - timedelta(days=today.weekday())
-            begin_midnight = "{}T00:00:00.0Z".format(last_monday)
-            coming_monday = today + timedelta(days=-today.weekday(), weeks=1)
-            end_midnight = "{}T00:00:00.0Z".format(coming_monday)
+        try:
+            if period == "daily":
+                begin_midnight = "{}T00:00:00.0Z".format(date.today())
+                end_midnight = "{}T00:00:00.0Z".format(date.today() + timedelta(days=1))
+            else:
+                today = date.today()
+                last_monday = today - timedelta(days=today.weekday())
+                begin_midnight = "{}T00:00:00.0Z".format(last_monday)
+                coming_monday = today + timedelta(days=-today.weekday(), weeks=1)
+                end_midnight = "{}T00:00:00.0Z".format(coming_monday)
 
-        attendances_ids = self.handle.execute('hr.attendance', 'search',
-                                              [('employee_id', '=', self.employee_id),
-                                               ("check_in", ">=", begin_midnight),
-                                               ("check_in", "<", end_midnight)
-                                               ])
-        attendances_details = self.handle.execute('hr.attendance', 'read', attendances_ids)
-        total_time_today = 0
-        for i in attendances_details:
-            checkin = i['check_in']  # str '2020-12-22 15:44:11' /!\ in GMT
-            checkout = i['check_out'] if i['check_out'] is not False else \
-                datetime.datetime.strftime(datetime.datetime.now(pytz.UTC), "%Y-%m-%d %H:%M:%S")
+            attendances_ids = self.handle.execute('hr.attendance', 'search',
+                                                  [('employee_id', '=', self.employee_id),
+                                                   ("check_in", ">=", begin_midnight),
+                                                   ("check_in", "<", end_midnight)
+                                                   ])
+            attendances_details = self.handle.execute('hr.attendance', 'read', attendances_ids)
+            total_time_today = 0
+            for i in attendances_details:
+                checkin = i['check_in']  # str '2020-12-22 15:44:11' /!\ in GMT
+                checkout = i['check_out'] if i['check_out'] is not False else \
+                    datetime.strftime(datetime.now(pytz.UTC), "%Y-%m-%d %H:%M:%S")
 
-            checkin_dt = datetime.strptime(checkin, '%Y-%m-%d %H:%M:%S')
-            checkout_dt = datetime.strptime(checkout, '%Y-%m-%d %H:%M:%S')
-            total_time_today += (checkout_dt - checkin_dt).total_seconds()
-        return timedelta(seconds=total_time_today)
+                checkin_dt = datetime.strptime(checkin, '%Y-%m-%d %H:%M:%S')
+                checkout_dt = datetime.strptime(checkout, '%Y-%m-%d %H:%M:%S')
+                total_time_today += (checkout_dt - checkin_dt).total_seconds()
+            return timedelta(seconds=total_time_today)
+        except urllib.error.URLError as e:
+            logging.error("Unable to connect to Odoo", e)
+            return timedelta(seconds=0)
