99388b257446a499410d8e87894ee0ed14090136
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2020-12-28 15:18:14 +0100

    magichue - sometimes the connection to the bulb is lost

--- modules/wifi_lamp.py
+++ modules/wifi_lamp.py
@@ -4,15 +4,25 @@ import magichue
 class wifi_lamp:
 
     def __init__(self, ipaddr):
-        self.light = magichue.Light(ipaddr)
+        self._ipaddr = ipaddr
+        self.light = magichue.Light(self._ipaddr)
+
+    def _recycle(self):
+        self.light = magichue.Light(self._ipaddr)
 
     def alert(self, speed, color):
+        self._recycle()
         self.light.speed = speed
-        if self.light.is_white:
-            self.light.is_white = False
-            self.light.mode = color
-        if not self.light.on:
-            self.light.on = True
+        self.light.is_white = False
+        self.light.mode = color
+        self.light.on = True
+
+    def white(self, brightness=255):
+        self._recycle()
+        self.light.is_white = True
+        self.light.brightness = brightness
+        self.light.on = True
+
 
     def red_alert(self, speed=1.0):
         return self.alert(speed, magichue.RED_GRADUALLY)
@@ -21,4 +31,5 @@ class wifi_lamp:
         return self.alert(speed, magichue.GREEN_GRADUALLY)
 
     def off(self):
+        self._recycle()
         self.light.on = False
